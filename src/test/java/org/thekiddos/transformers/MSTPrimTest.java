78e99534232a1d9dae54310d3612e1270a65511e
J2mF <mp4-12cs5@hotmail.com>
2022-05-20 11:34:07 +0300

    Fix assert semantics in MSTPrimTest

--- src/test/java/org/thekiddos/transformers/MSTPrimTest.java
+++ src/test/java/org/thekiddos/transformers/MSTPrimTest.java
@@ -39,73 +39,73 @@ class MSTPrimTest {
 
         var tree = new MSTPrim( graph, 0 ).transform();
         assertEquals(
-                tree.getOutEdges( 0 ),
                 List.of(
                         new Edge( 0, 1, 4 ),
                         new Edge( 0, 2, 8 )
-                )
+                ),
+                tree.getOutEdges( 0 )
         );
 
         assertEquals(
-                tree.getOutEdges( 1 ),
                 List.of(
                         new Edge( 1, 0, 4 )
-                )
+                ),
+                tree.getOutEdges( 1 )
         );
 
         assertEquals(
-                tree.getOutEdges( 2 ),
                 List.of(
                         new Edge( 2, 0, 8 ),
                         new Edge( 2, 5, 1 )
-                )
+                ),
+                tree.getOutEdges( 2 )
         );
 
         assertEquals(
-                tree.getOutEdges( 3 ),
                 List.of(
                         new Edge( 3, 8, 4 ),
                         new Edge( 3, 4, 2 ),
                         new Edge( 3, 6, 7 )
-                )
+                ),
+                tree.getOutEdges( 3 )
         );
 
         assertEquals(
-                tree.getOutEdges( 4 ),
                 List.of(
                         new Edge( 4, 3, 2 )
-                )
+                ),
+                tree.getOutEdges( 4 )
         );
 
         assertEquals(
-                tree.getOutEdges( 5 ),
                 List.of(
                         new Edge( 5, 2, 1 ),
                         new Edge( 5, 8, 2 )
-                )
+                ),
+                tree.getOutEdges( 5 )
         );
 
         assertEquals(
-                tree.getOutEdges( 6 ),
                 List.of(
                         new Edge( 6, 3, 7 ),
                         new Edge( 6, 7, 9 )
-                )
+                ),
+                tree.getOutEdges( 6 )
         );
 
         assertEquals(
-                tree.getOutEdges( 7 ),
                 List.of(
                         new Edge( 7, 6, 9 )
-                )
+                ),
+                tree.getOutEdges( 7 )
         );
 
         assertEquals(
-                tree.getOutEdges( 8 ),
                 List.of(
                         new Edge( 8, 5, 2 ),
                         new Edge( 8, 3, 4 )
-                )
+                ),
+                tree.getOutEdges( 8 )
         );
     }
 }
